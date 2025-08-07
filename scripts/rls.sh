36ab323d084d78491814ead1c71af5996ae249d4
ed <s@ocv.me>
2025-08-07 21:26:59 +0000

    sfx: simplify (remove bruteforce packing)

--- scripts/rls.sh
+++ scripts/rls.sh
@@ -32,11 +32,7 @@ v=$1
 rm -f ../dist/copyparty-sfx*
 shift
 ./make-sfx.sh "$@"
-f=../dist/copyparty-sfx
-[ -e $f.py ] && s= || s=-gz
-# TODO: the -gz suffix is gone, can drop all the $s stuff probably
-
-$f$s.py --version >/dev/null
+../dist/copyparty-sfx.py --version >/dev/null
 
 while [ "$1" ]; do
     case "$1" in
@@ -44,29 +40,6 @@ while [ "$1" ]; do
         fast) break;;
     esac
     shift
-done
-
-[ $parallel -gt 1 ] && {
-    printf '\033[%s' s 2r H "0;1;37;44mbruteforcing sfx size -- press enter to terminate" K u "7m $* " K $'27m\n'
-    trap "rm -f .sfx-run; printf '\033[%s' s r u" INT TERM EXIT
-    touch .sfx-run
-    min=99999999
-    for ((a=0; a<$parallel; a++)); do
-        while [ -e .sfx-run ]; do
-            CSN=$a ./make-sfx.sh re "$@"
-            sz=$(wc -c <$f$a$s.py | awk '{print$1}')
-            [ $sz -ge $min ] && continue
-            mv $f$a$s.py $f$s.py.$sz
-            min=$sz
-        done &
-    done
-    read
-    exit
-}
-
-while true; do
-    mv $f$s.py $f$s.$(wc -c <$f$s.py | awk '{print$1}').py
-    ./make-sfx.sh re "$@"
 done
 
 # git tag -d v$v; git push --delete origin v$v
